9df945ba35e4c6cf278b90b63ed05dce0e998129
mohit kumar <155976124+mohitkumar743@users.noreply.github.com>
2024-08-31 16:33:50 +0530

    Update navbar.jsx

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <div className="text-center text-white h-[10vh] flex flex-row items-center justify-around">
       <div className="font-extrabold"> <Link to="/">Q M S</Link></div>
       <div className="">
-      <Link to="/admin"><button className="btn text-white btn-outline m-3 w-[100px] ">admin Login</button></Link>
+{/*       <Link to="/admin"><button className="btn text-white btn-outline m-3 w-[100px] ">admin Login</button></Link> */}
       <Link to="/Login"><button className="btn text-white btn-outline m-3 w-[100px] ">Login</button></Link>
       <Link to="/signup"><button className="btn text-white btn-outline m-3 w-[100px] ">SignUp</button></Link>
       
